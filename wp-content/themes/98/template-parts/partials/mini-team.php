ba4da76fd06f04aad120ceb4984ab7e6c718931a
programmer-man <bryan@kerigan.com>
2018-08-08 18:55:34 -0400

    team tiles

--- wp-content/themes/98/template-parts/partials/mini-team.php
+++ wp-content/themes/98/template-parts/partials/mini-team.php
@@ -1,5 +1,5 @@
 <?php $agent = (isset($agent) ? $agent : null); ?>
-<div class="col-sm-6 col-lg-4 col-xl-3" >
+<div class="col-sm-6 col-lg-4" >
     <div class="agent-container">
         <div class="agent-thumb text-xs-center">
             <a href="<?= $agent['link']; ?>">
@@ -11,7 +11,11 @@
             <p class="title"><?php echo $agent['title']; ?></p>
             <div class="line"></div>
             <?php if($agent['email']!=''){ ?><p class="email"><a href="mailto:<?php echo $agent['email']; ?>" ><?php echo $agent['email']; ?></a></p><?php } ?>
-            <?php if($agent['cell_phone']!=''){ ?><p class="phone clicktocall"><a href="tel:<?php echo $agent['cell_phone']; ?>" ><?php echo $agent['cell_phone']; ?></a></p><?php } ?>
+            <?php if($agent['cell_phone']!=''){ ?>
+                <p class="phone clicktocall"><a href="tel:<?php echo $agent['cell_phone']; ?>" ><?php echo $agent['cell_phone']; ?></a></p>
+            <?php }elseif($agent['office_phone']){ ?>
+                <p class="phone clicktocall"><a href="tel:<?php echo $agent['office_phone']; ?>" ><?php echo $agent['office_phone']; ?></a></p>
+            <?php } ?>
             <a href="<?php echo $agent['link']; ?>" class="btn btn-primary agent-button" >more info</a>
         </div>
     </div>
