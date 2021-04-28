72a6376cfa0ffd5942aab685f4a9020b1a1aaad3
ToniSHernandez <toni@kerigan.com>
2021-04-28 12:33:19 -0500

    Fixed missing Site By in Footer.

--- wp-content/themes/98/footer.php
+++ wp-content/themes/98/footer.php
@@ -39,7 +39,7 @@
 	<div id="bot-bot">
 		<div class="container no-gutter">
 			<div class="row">
-				<div class="col-xs-12 col-sm-4">
+				<div class="col-xs-4 col-sm-4">
 					<div class="social">
 					<?php
 						$socialLinks = getSocialLinks();
@@ -49,14 +49,14 @@
 					?>
 					</div>
 				</div>
-				<div class="col-xs-12 col-sm-4">
+				<div class="col-xs-4 col-sm-4">
 					<div class="site-info text-xs-center">
 						<p class="copyright">&copy;<?php echo date('Y'); ?> 98 Real Estate Group. All Rights Reserved.</p>
 					</div>
 				</div>
-				<div class="col-xs-12 col-sm-4">
+				<div class="col-xs-4 col-sm-4">
 					<div class="site-info text-xs-center">
-						<p class="siteby pull-sm-right" style="display: none;"><svg version="1.1" id="kma" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 12.5 8.7" style="enable-background:new 0 0 12.5 8.7;" xml:space="preserve">
+						<p class="siteby pull-sm-right"><svg version="1.1" id="kma" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 12.5 8.7" style="enable-background:new 0 0 12.5 8.7;" xml:space="preserve">
 						<path class="st0" d="M6.4,0.1c0,0,0.1,0.3,0.2,0.9c1,3,3,5.6,5.7,7.2l-0.1,0.5c0,0-0.4-0.2-1-0.4C7.7,7,3.7,7,0.2,8.5L0.1,8.1
 							c2.8-1.5,4.8-4.2,5.7-7.2C6,0.4,6.1,0.1,6.1,0.1H6.4L6.4,0.1z"/>
 						</svg> Site by <a href="https://keriganmarketing.com">KMA</a>.</p>
